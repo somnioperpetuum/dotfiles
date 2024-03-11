8066ab2955b02143b069ed6ed85aa875e67e7776
Somnio <somnioperpetuum@proton.me>
2024-03-11 17:47:26 -0400

    Installer Utility v0.1 Linux Testing

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
-#!usr/bin/bash
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install python3 -y && sudo apt install pip3 -y
+
+source ~/.profile
+
+python3 -m venv .dotfiles
+
+cd .dotfiles
+
+source .dotfiles/bin/activate
+
+pip3 install -r ./requirements.txt
+
+python3 ./install.py
