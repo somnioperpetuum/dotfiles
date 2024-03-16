846c0456cb7ee8793b35ef8e40f6ff5cd4c60431
Somnio <somnioperpetuum@proton.me>
2024-03-15 20:06:47 -0400

    Installer Utility v0.9 Linux Support. Testing

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install python3 python3-pip git -y 
 
-source ~/.profile
-
-pip3 install -r ./requirements.txt
+pip3 install -r /home/$USER/dotfiles/requirements.txt
 
 python3 ./main.py
